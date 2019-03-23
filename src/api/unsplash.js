c05b846567b962301a35b52d252b59899215ee72
J <socept@gmail.com>
2019-03-23 15:54:11 -0400

    changed API

--- src/api/unsplash.js
+++ src/api/unsplash.js
@@ -4,6 +4,6 @@ export default axios.create({
     baseURL: 'https://api.unsplash.com',
     headers: {
         Authorization: 
-        'Client-ID a2f938d9f839d4d05c8043b9f0345dc2d20a9542191a3e8eab66b70fb9e70a1f'
+        'Client-ID YOUR_API_KEY'
     }
 });
